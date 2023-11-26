77c55bc77f72bea4551beaadafc9b964521df716
Byauric <150602030+Byauric@users.noreply.github.com>
2023-11-26 03:39:59 +0000

    subiendo ejercicio 1

---
+++ Ejercicio1.py
@@ -0,0 +1,10 @@
+#Problema1: 
+"""Escribe un programa en Python para encontrar los números que son divisibles por 7 y múltiplos de 5, 
+en el rango de 1500 y 2700 (ambos incluidos)."""
+
+resultados = []
+for numero in range(1500, 2701):
+    if numero % 7 == 0 and numero % 5 == 0:
+        resultados.append(numero)
+print("Los números divisibles por 7 y múltiplos de 5 en el rango de 1500 a 2700 son:")
+print(resultados)
---
+++ Ejercicio4.py
@@ -0,0 +1,24 @@
+#Problema 4:
+"""Imaginemos que lo han contratado para un colegio donde se desea realizar un sistema por el cual se
+pueda generar un listado de “n” alumnos y 3 calificaciones que corresponden a alguna de sus materias.
+Puede usar el siguiente esquema a manera de ejemplo
+{
+Alumno: Juan,
+Notas: [10, 12, 15]
+} Una vez completado el ingreso de los datos, su programa debe mostrar en pantalla el listado
+completo de los alumnos.
+Nota:
+El uso de listas con diccionarios le será de utilidad."""
+
+lista_alumnos = []
+
+while True:
+    nombre = input("Ingrese el nombre del alumno (o escriba 'fin' para terminar): ")
+    if nombre.lower() == 'fin':
+        break
+    notas = [float(input(f"Ingrese la calificación {i+1} del alumno {nombre}: ")) for i in range(3)]
+    lista_alumnos.append({"Alumno": nombre, "Notas": notas})
+
+print("\nListado completo de alumnos:")
+for alumno in lista_alumnos:
+    print(f"Alumno: {alumno['Alumno']}, Notas: {alumno['Notas']}")
