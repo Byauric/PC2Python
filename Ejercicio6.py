e92c39c50873a0fd853abfbad336a6b43cc9ce53
Byauric <150602030+Byauric@users.noreply.github.com>
2023-11-26 04:27:38 +0000

    subiendo ejercicio 6

--- Ejercicio6.py
+++ Ejercicio6.py
@@ -3,3 +3,12 @@
 Nota: La secuencia de Fibonacci es la serie de números:
 0, 1, 1, 2, 3, 5, 8, 13, 21,. ...
 Cada número siguiente se obtiene sumando los dos números anteriores."""
+
+def fibonacci_hasta_50():
+    a, b = 0, 1
+    while a <= 50:
+        print(a, end=", ")
+        a, b = b, a + b
+fibonacci_hasta_50()
+
+
--- Ejercicio7.py
+++ Ejercicio7.py
@@ -1,3 +1,19 @@
 #Problema 7:
 """Escriba una función de Python que tome un número como parámetro y verifique que el número sea
 primo o no."""
+
+def es_primo(numero):
+    if numero <= 1:
+        return False
+    elif numero <= 3:
+        return True
+    else:
+        for i in range(2, int(numero**0.5) + 1):
+            if numero % i == 0:
+                return False
+        return True
+numero_a_verificar = int(input("Ingrese un número para verificar si es primo: "))
+if es_primo(numero_a_verificar):
+    print(f"{numero_a_verificar} es un número primo.")
+else:
+    print(f"{numero_a_verificar} no es un número primo.")
