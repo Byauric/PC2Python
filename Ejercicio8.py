6cdc640040184cf0cf68e5d54f280e5baf169f39
Byauric <150602030+Byauric@users.noreply.github.com>
2023-11-26 04:46:21 +0000

    subiendo ejercicio 8

--- Ejercicio8.py
+++ Ejercicio8.py
@@ -1,3 +1,13 @@
 #Problema 8:
 """Escribe una función de Python para calcular el factorial de un número (un entero no negativo). La
 función acepta el número como argumento."""
+
+def factorial_numero(num):
+    factorial = 1
+    for i in range(num):
+        factorial*=(i+1)
+        return factorial
+n = int(input('Ingrese el número: '))
+resultado = factorial_numero(n)
+print('El numero factorial es: ', resultado)
+
