8a3aa5dc50218a0b335473fdfacb389cc9b77f34
Byauric <150602030+Byauric@users.noreply.github.com>
2023-11-26 04:14:58 +0000

    subiendo ejercicio 5

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -7,3 +7,13 @@ Cantidad de veces 0 en el número: 4
 Nota: Para resolver este problema, algunos tipos de datos dentro de python contemplan un método
 count."""
 
+def contar_digitos(numero, digito):
+    numero_str = str(numero)
+    cantidad = numero_str.count(str(digito))
+    return cantidad
+
+numero_ingresado = int(input("Ingrese un número entero: "))
+digito_solicitado = int(input("Ingrese un dígito: "))
+
+resultado = contar_digitos(numero_ingresado, digito_solicitado)
+print(f"Cantidad de veces {digito_solicitado} en el número: {resultado}")
